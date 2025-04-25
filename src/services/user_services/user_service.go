fba7319dc167d0343222a3b48864fe4237db23d5
ahmadsaubani <banz.cozy@gmail.com>
2025-04-25 13:47:26 +0700

    wrap err user service

--- src/services/user_services/user_service.go
+++ src/services/user_services/user_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"gin/src/entities/users"
 	repositories "gin/src/repositories/user_repositories"
 )
@@ -20,12 +21,12 @@ func NewUserService(repo repositories.UserRepository) UserService {
 func (s *userService) GetPaginatedUsers(limit, offset int, order string) ([]users.User, int64, error) {
 	usersList, err := s.repo.GetAll(limit, offset, order)
 	if err != nil {
-		return nil, 0, err
+		return nil, 0, fmt.Errorf("sorry, we encountered an issue fetching the user list. Please try again later: %w", err)
 	}
 
 	total, err := s.repo.CountAll()
 	if err != nil {
-		return nil, 0, err
+		return nil, 0, fmt.Errorf("sorry, we couldn't count the users at the moment. Please try again later: %w", err)
 	}
 
 	return usersList, total, nil
